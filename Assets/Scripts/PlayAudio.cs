8a6e3169bd756e7b0d9be2ca40c4f5cf4fc3f79c
Alyisha Bouges <45861884+abouges@users.noreply.github.com>
2020-04-21 18:53:16 -0400

    New repository
    hello?

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PlayAudio : MonoBehaviour
+{
+    public GameObject play_sound;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Play()
+    {
+        print("YOU'VE BEEN SEEN!");
+
+        play_sound.GetComponent<AudioSource>().Play();
+    }
+
+    public void Stop()
+    {
+        print("EY! WHEREDY GOOO?!");
+
+        play_sound.GetComponent<AudioSource>().Stop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
